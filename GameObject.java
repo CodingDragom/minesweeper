6ba7fb88f2fb1890e3cff1207607c943eab7ef06
CodingDragom <justinsedrak@gmail.com>
2024-08-24 13:22:35 -0700

    Create GameObject.java

---
+++ GameObject.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class GameObject {
+    protected int rows;
+    protected int cols;
+
+    public GameObject(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public void setCols(int cols) {
+        this.cols = cols;
+    }
+}
